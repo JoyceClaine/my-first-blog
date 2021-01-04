e51fa1b94a2de41498077d9cde5969cd1398d476
JoyceClaine <joyce_claine@hotmail.com>
2021-01-04 15:23:16 -0300

    Mudanças no HTML da página.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
+#As views servem para conectar os modelos e os templates
+
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+#O ponto antes de models significa diretório atual ou aplicativo atual. Tanto views.py como models.py estão no mesmo diretório.
+#Isto significa que podemos usar . e o nome do arquivo (sem py).
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
